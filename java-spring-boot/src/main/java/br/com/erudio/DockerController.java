17a791ea4755b4220224ce2254d282f4ec399cee
leandrocgsi <leandrocgsi@gmail.com>
2021-05-21 20:13:49 -0300

    Adding Docker

--- java-spring-boot/src/main/java/br/com/erudio/DockerController.java
+++ java-spring-boot/src/main/java/br/com/erudio/DockerController.java
@@ -1,13 +1,19 @@
 package br.com.erudio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerController {
 	
+	Logger logger = LoggerFactory.getLogger(DockerController.class);
+	
 	@RequestMapping("/hello-docker")
 	public HelloDocker greeting() {
+		
+		logger.info("Endpoint /hello-docker is called!!!");
 		
 		//var hostName = System.getenv("COMPUTERNAME");
 		var hostName = System.getenv("HOSTNAME");
