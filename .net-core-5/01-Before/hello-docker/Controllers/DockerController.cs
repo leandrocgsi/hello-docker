139d31c684d2510d7294389e18f93765518abb7f
leandrocgsi <leandrocgsi@gmail.com>
2021-06-08 17:40:14 -0300

    Initial .NET 5 Commit

---
+++ .net-core-5/01-Before/hello-docker/Controllers/DockerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace hello_docker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DockerController : ControllerBase
+    {
+
+        private readonly ILogger<DockerController> _logger;
+
+        public DockerController(ILogger<DockerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/hello-docker")]
+        public HelloDocker greeting()
+        {
+            _logger.LogInformation("Endpoint /hello-docker is called!!!");
+            //var hostName = Environment.GetEnvironmentVariable("COMPUTERNAME");
+            var hostName = Environment.GetEnvironmentVariable("HOSTNAME");
+
+            return new HelloDocker(
+                        "Hello Docker",
+                        hostName
+                    );
+        }
+    }
+}
