6cf500ab79700238cf40c453a511f3187ab3db91
leandrocgsi <leandrocgsi@gmail.com>
2022-09-21 10:46:20 -0300

    Adding Docker to Spring Boot and Kotlin App

--- kotlin/01-Before/hello-docker/src/main/kotlin/br/com/erudio/DockerController.kt
+++ kotlin/01-Before/hello-docker/src/main/kotlin/br/com/erudio/DockerController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class DockerController {
-    
+
     @RequestMapping("/hello-docker")
     fun greeting(): HelloDocker {
 
